793c86449441422e22cc2a968d3d4620d31062ed
Ivan <ivan@webba-booking.com>
2023-10-25 11:36:05 +0200

    Typo fixes

--- tests/test-slideshow-admin-scripts-styles.php
+++ tests/test-slideshow-admin-scripts-styles.php
@@ -25,16 +25,16 @@ class SlideshowAdminScripsStylesTest extends WP_UnitTestCase {
         $this->assertTrue(  wp_style_is( 'slideshow-admin', 'registered' ) );
 
         do_action('admin_enqueue_scripts');
-        // script is not enqueued becasue we are on the 'dashboard' screen
+        // script is not enqueued because we are on the 'dashboard' screen
         $this->assertFalse(  wp_script_is( 'slideshow-admin' ) );
-        // style is not enqueued becasue we are on the 'dashboard' screen
+        // style is not enqueued because we are on the 'dashboard' screen
         $this->assertFalse(  wp_style_is( 'slideshow-admin' ) );
 
         set_current_screen( 'toplevel_page_slide-show-plugin-settings' );   
         do_action('admin_enqueue_scripts');
-        // script is enqueued becasue we are the plugin settings screen
+        // script is enqueued because we are the plugin settings screen
         $this->assertTrue(  wp_script_is( 'slideshow-admin' ) );
-        // style is enqueued becasue we are the plugin settings screen
+        // style is enqueued because we are the plugin settings screen
         $this->assertTrue(  wp_style_is( 'slideshow-admin' ) );
     }
 }
